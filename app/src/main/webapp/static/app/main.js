260a302d15f5e40538e6cd8ad9e8b05145b50472
Sadika Sumanapala <sadikahs@gmail.com>
2013-10-28 00:42:54 +0530

    clean up - removed non existant files/paths

--- app/src/main/webapp/static/app/main.js
+++ app/src/main/webapp/static/app/main.js
@@ -3,23 +3,8 @@ angular.module("main-app", ["$strap.directives", "ngTable"], function($routeProv
   $routeProvider.when("/", {
     templateUrl: "./static/app/templates/index.html"
   });
-  $routeProvider.when("/test", {
-    templateUrl: "./static/app/templates/test.html"
-  });
   $routeProvider.when("/login", {
     templateUrl: "./static/app/templates/login.html"
-  });
-  $routeProvider.when("/contactDetails", {
-    templateUrl: "./static/app/templates/contactDetails.html"
-  });
-  $routeProvider.when("/registration", {
-    templateUrl: "./static/app/templates/registration.html"
-  });
-  $routeProvider.when("/editUser", {
-    templateUrl: "./static/app/templates/editUser.html"
-  });
-  $routeProvider.when("/customer/registration", {
-    templateUrl: "./static/app/templates/customer/registration.html"
   });
   $routeProvider.when("/RegistrationWizard", {
     templateUrl: "./static/app/templates/RegistrationWizard.html"
