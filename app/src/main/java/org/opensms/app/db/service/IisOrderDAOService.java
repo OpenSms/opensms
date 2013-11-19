bfdfcc56facdefbd0f5d73d7c0b5b35dce7fab3a
Sadika Sumanapala <sadikahs@gmail.com>
2013-11-19 12:43:34 +0530

    save iss order with selected pre orders

--- app/src/main/java/org/opensms/app/db/service/IisOrderDAOService.java
+++ app/src/main/java/org/opensms/app/db/service/IisOrderDAOService.java
@@ -2,12 +2,19 @@ package org.opensms.app.db.service;
 
 import org.opensms.app.db.controller.BatchDAO;
 import org.opensms.app.db.controller.IisOrderDAO;
+import org.opensms.app.db.controller.IisOrderHasBatchDAO;
+import org.opensms.app.db.controller.impl.PreOrderDAOController;
+import org.opensms.app.db.entity.*;
 import org.opensms.app.view.model.IisOrderModel;
+import org.opensms.app.view.model.ItemModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,12 +29,27 @@ public class IisOrderDAOService {
     @Autowired
     private IisOrderDAO iisOrderDAO;
 
-    public void saveIisOrder(IisOrderModel iisOrderModel) {
-        iisOrderModel.getIisOrder().setIssOrderDateTime(Calendar.getInstance().getTime());
+    @Autowired
+    private IisOrderHasBatchDAO iisOrderHasBatchDAO;
 
+    @Autowired
+    private BatchDAO batchDAO;
+
+    @Autowired
+    private PreOrderDAOController preOrderDAOController;
+
+    public void saveIisOrder(IisOrderModel iisOrderModel) {
+        IisOrder iisOrder = iisOrderModel.getIisOrder();
+
+        iisOrder.setIssOrderDateTime(Calendar.getInstance().getTime());
         iisOrderDAO.save(iisOrderModel.getIisOrder());
 
-        iisOrderModel.getIisOrder().getIisOrderId();
-
+        // update pre orders. set iis order and close it.
+        List<PreOrder> preOrderList = iisOrderModel.getPreOrderList();
+        for (PreOrder p : preOrderList) {
+            p.setIisOrder(iisOrder);
+            p.setIsOpen(false);
+            preOrderDAOController.update(p);
+        }
     }
 }
