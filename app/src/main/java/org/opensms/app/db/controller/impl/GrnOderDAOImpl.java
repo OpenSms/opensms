2061c842a46944f8ed164410bfe30317f15b6a21
Sadika Sumanapala <sadikahs@gmail.com>
2014-01-05 12:43:44 +0530

    vendor grn order report retrieving basic data

--- app/src/main/java/org/opensms/app/db/controller/impl/GrnOderDAOImpl.java
+++ app/src/main/java/org/opensms/app/db/controller/impl/GrnOderDAOImpl.java
@@ -1,9 +1,14 @@
 package org.opensms.app.db.controller.impl;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.opensms.app.db.controller.AbstractDAO;
 import org.opensms.app.db.controller.GrnOrderDAO;
 import org.opensms.app.db.entity.GrnOrder;
+import org.opensms.app.db.entity.Vendor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,5 +21,33 @@ import org.springframework.stereotype.Repository;
 public class GrnOderDAOImpl extends AbstractDAOImpl<GrnOrder,Long> implements GrnOrderDAO {
     public GrnOderDAOImpl() {
         super(GrnOrder.class, Long.class);
+    }
+
+    @Override
+    public List<GrnOrder> getAllGrnOrdersOfCurrentVendor(Vendor vendor) {
+
+        Session session = getCurrentSession();
+        Query query = session.createQuery("SELECT g FROM GrnOrder g WHERE g.vendor=:vendor");
+        query.setParameter("vendor", vendor);
+
+        List<GrnOrder> list = null;
+        try {
+            list = query.list();
+//            for (GrnOrder grnOrder:list){
+//                Hibernate.initialize(grnOrder);
+//                for(GrnPayment grnPayment:grnOrder.getGrnPaymentList()){
+//                    Hibernate.initialize(grnPayment);
+//                }
+//
+//                for(Batch batch:grnOrder.getBatchList()){
+//                    Hibernate.initialize(batch);
+//                }
+//
+//            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
