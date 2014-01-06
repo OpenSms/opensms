605bb73e683ece0e9c7d925b1857814d3130bd3c
dewmal <dewmalnilanka@gmail.com>
2014-01-06 22:39:24 +0530

    Create GSR ORder

--- app/src/main/java/org/opensms/app/controller/GSROrderController.java
+++ app/src/main/java/org/opensms/app/controller/GSROrderController.java
@@ -1,18 +1,20 @@
 package org.opensms.app.controller;
 
-import org.opensms.app.db.entity.Batch;
-import org.opensms.app.db.entity.GsrOrder;
+import org.opensms.app.db.entity.IisOrder;
+import org.opensms.app.db.entity.IisOrderHasBatch;
+import org.opensms.app.db.entity.PreOrder;
+import org.opensms.app.db.entity.PreOrderHasItem;
 import org.opensms.app.db.service.GsrOrderDAOService;
+import org.opensms.app.db.service.IisOrderDAOService;
+import org.opensms.app.db.service.ItemDAOService;
+import org.opensms.app.db.service.PreOrderDAOService;
 import org.opensms.app.view.entity.GsrOrderModel;
+import org.opensms.app.view.entity.IISOrderHasBatch;
 import org.opensms.app.view.model.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -26,14 +28,45 @@ public class GSROrderController {
     @Autowired
     private GsrOrderDAOService gsrOrderDAOService;
 
+    @Autowired
+    private ItemDAOService itemDAOService;
 
-    @RequestMapping(value = "/save",method = RequestMethod.PUT)
-    public ResponseMessage createGsrOrder(@RequestBody GsrOrderModel gsrOrderModel){
-        Long id=gsrOrderDAOService.save(gsrOrderModel);
-        return new ResponseMessage(ResponseMessage.Type.success,"ok");
+    @Autowired
+    private PreOrderDAOService preOrderDAOService;
+
+
+    @Autowired
+    private IisOrderDAOService iisOrderDAOService;
+
+
+    @RequestMapping(value = "/save", method = RequestMethod.PUT)
+    public ResponseMessage createGsrOrder(@RequestBody GsrOrderModel gsrOrderModel) {
+        Long id = gsrOrderDAOService.save(gsrOrderModel);
+        return new ResponseMessage(ResponseMessage.Type.success, "ok");
     }
 
 
+    @RequestMapping(value = "/getpreorders", method = RequestMethod.GET, params = {"userid"})
+    @ResponseBody
+    public List<PreOrder> getPreOrders(@RequestParam("userid") String userid) {
+        return preOrderDAOService.getPreOrdersFrom(userid);
+    }
+
+
+    @RequestMapping(value = "/getpreorderitesm", method = RequestMethod.GET, params = {"preorderid"})
+    @ResponseBody
+    public List<PreOrderHasItem> getPreOrderHasItems(@RequestParam("preorderid") String preorderid) {
+        return preOrderDAOService.getPreOrderHasItemsOf(Long.parseLong(preorderid));
+    }
+
+
+
+    @RequestMapping(value = "/getpreorderitesm", method = RequestMethod.GET, params = {"salesEmp"})
+    @ResponseBody
+    public List<IisOrderHasBatch> getIisOrderHasBatches(@RequestParam("salesEmp") String salesEmp) {
+        IisOrder openOrder = iisOrderDAOService.getOpenOrder(salesEmp);
+        return iisOrderDAOService.getBatchList(openOrder.getIisOrderId()+"");
+    }
 
 
 }
